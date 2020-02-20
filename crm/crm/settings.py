1c6584634737e25ecf4aee0ce5f0c3814140219d
abylgazievaalt <abylgazievaalt@gmail.com>
2020-02-20 15:23:09 +0600

    changed installed apps

--- crm/crm/settings.py
+++ crm/crm/settings.py
@@ -53,10 +53,11 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
-    'app.apps.AppConfig',
+    # 'app.apps.AppConfig',
     'rest_framework_jwt',
     'crispy_forms',
     'Deal',
+    'app',
 ]
 
 CRISPY_TEMPLATE_PACK = 'bootstrap4'
@@ -91,24 +92,26 @@ TEMPLATES = [
     },
 ]
 
+# ----------------------------------------------------
 WSGI_APPLICATION = 'crm.wsgi.application'
+# ----------------------------------------------------
 
 
 # Database
 # https://docs.djangoproject.com/en/2.1/ref/settings/#databases
 
-DATABASES = {
-    'default': dj_database_url.config(
-        default=config('DATABASE_URL')
-    )
-}
-
 # DATABASES = {
-#     'default': {
-#         'ENGINE': 'django.db.backends.sqlite3',
-#         'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
-#     }
+#     'default': dj_database_url.config(
+#         default=config('DATABASE_URL')
+#     )
 # }
+
+DATABASES = {
+    'default': {
+        'ENGINE': 'django.db.backends.sqlite3',
+        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+    }
+}
 
 
 # Password validation
@@ -149,10 +152,7 @@ USE_TZ = True
 
 PROJECT_ROOT = os.path.dirname(os.path.abspath(__file__))
 
-# Static files (CSS, JavaScript, Images)
-# https://docs.djangoproject.com/en/1.9/howto/static-files/ STATIC_ROOT = os.path.join(PROJECT_ROOT, 'staticfiles')
 STATIC_URL = '/static/'
-# Extra places for collectstatic to find static files.
 STATICFILES_DIRS = (
     os.path.join(PROJECT_ROOT, 'static'),
 )

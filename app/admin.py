1de5953a98a745cfa52a8ecf9b0d414079216ddc
abylgazievaalt <abylgazievaalt@gmail.com>
2020-03-02 21:25:57 +0600

    added manager's admin

--- app/admin.py
+++ app/admin.py
@@ -1,3 +1,4 @@
+from django.contrib.admin import AdminSite
 from django.contrib.auth.admin import UserAdmin
 
 from .forms import CustomUserChangeForm
@@ -6,7 +7,7 @@ from django.contrib.auth.models import Group
 from .models import Individual, UserStatus, Driver, TradingSet, Manager, User
 from django.contrib import admin
 from .forms import SignupForm
-
+from Deal.models import Order
 
 class CustomUserAdmin(UserAdmin):
     add_form = SignupForm
@@ -39,3 +40,25 @@ admin.site.register(Individual)
 admin.site.register(TradingSet)
 admin.site.register(Manager)
 admin.site.unregister(Group)
+
+
+class ManagerAdminSite(AdminSite):
+    site_header = "CRM Logistics Managers"
+    site_title = "CRM Logistics Events Managers"
+    index_title = "Welcome to CRM Logistics"
+
+manager_admin_site = ManagerAdminSite(name='manager_admin')
+
+
+class ManagerAdmin(admin.ModelAdmin):
+    def has_add_permission(self, request):
+        return False
+
+    def has_delete_permission(self, request, obj=None):
+        return False
+
+    def has_change_permission(self, request, obj=None):
+        return False
+
+manager_admin_site.register(TradingSet, ManagerAdmin)
+manager_admin_site.register(Order)

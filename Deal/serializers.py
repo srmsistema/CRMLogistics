a6ce66e3d1526b57e138a908cd7e79aac138f95f
abylgazievaalt <abylgazievaalt@gmail.com>
2020-03-25 22:55:26 +0600

    modified serializers and views (merged)

--- Deal/serializers.py
+++ Deal/serializers.py
@@ -86,3 +86,49 @@ class OrderSerializer(serializers.ModelSerializer):
     class Meta:
         model = Order
         fields = '__all__'
+
+
+class OrderDriverListSerializer(serializers.ModelSerializer):
+
+    volume = VolumeSerializer(many=False)
+    parametresTrailer = ParametresTrailerSerializer(many=False)
+    locationCargo = LocationCargoSerializer(many=False)
+
+    class Meta:
+        model = Order
+        exclude = ['companyProfit']
+
+class OrderDriverUpdateSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Order
+        fields = ['numberOrder', 'priceClient', 'dateLoading', 'dateUnloading', 'autoReleaseYear', 'countPallet',
+                  'stateAwning', 'requirementsLoading', 'typeAuto', 'typeLoading', 'typeCargo', 'weight',
+                  'volume', 'parametresTrailer', 'locationCargo',
+                  'user', 'driver', 'orderStatus', 'parametresTrailer', 'fromOrder', 'toOrder']
+        read_only_fields = fields
+
+class OrderClientCreateSerializer(serializers.ModelSerializer):
+
+    volume = VolumeSerializer()
+    parametresTrailer = ParametresTrailerSerializer()
+    locationCargo = LocationCargoSerializer()
+
+
+    class Meta:
+        model = Order
+        fields = ['numberOrder', 'priceClient', 'dateLoading', 'dateUnloading', 'autoReleaseYear', 'countPallet', 'stateAwning', 'requirementsLoading',
+                  'typeAuto', 'typeLoading', 'typeCargo', 'weight', 'volume', 'parametresTrailer', 'locationCargo',  'user']
+        read_only_fields = ['user', 'driver', 'orderStatus', 'parametresTrailer', 'fromOrder', 'toOrder', 'companyProfit']
+
+    def create(self, validated_data):
+        volume_data = validated_data.pop('volume')
+        parametresTrailer_data = validated_data.pop('parametresTrailer')
+        locationCargo_data = validated_data.pop('locationCargo')
+
+        volume = Volume.objects.create(**volume_data)
+        parametresTrailer = ParametresTrailer.objects.create(**parametresTrailer_data)
+        locationCargo = LocationCargo.objects.create(**locationCargo_data)
+
+        order = Order.objects.create(volume=volume, parametresTrailer=parametresTrailer, locationCargo=locationCargo,**validated_data)
+
+        return order

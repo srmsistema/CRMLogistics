7f1af42235b621b198558edddfb283f78ad12132
Nurzhan Ababakirov <nurzhanababakirov@MacBook-Pro-2.local>
2020-03-27 22:12:17 +0600

    Registration

---
+++ CRMClient/Controllers/ClientProfileViewController.swift
@@ -0,0 +1,30 @@
+//
+//  ClientProfileViewController.swift
+//  CRMClient
+//
+//  Created by Nurzhan Ababakirov on 3/26/20.
+//  Copyright © 2020 Nurzhan Ababakirov. All rights reserved.
+//
+
+import UIKit
+
+class ClientProfileViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
